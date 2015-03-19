6284171179a58f529263adeceaaf9047d3c4e16b
mgueriau <maxime.gueriau@gmail.com>
2015-03-19 13:33:07 +0100

    Modif mineure

---
+++ source/_static/php/corrections/tableau/index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Ma première page en PHP !</title>
+  </head>
+  <body>
+<?php
+$commande = array (
+'prix_unitaire' => 12,
+'quantite' => 5
+);
+
+$total = $commande['quantite'] * $commande['prix_unitaire'];
+echo "<p>Vous avez commandé ".$commande['quantite']." unités à ".$commande['prix_unitaire']."€ pièce; Soit un total de ".$total."€</p>";
+?>
+  </body>
+</html>
