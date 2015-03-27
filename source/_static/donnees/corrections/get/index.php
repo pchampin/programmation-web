0edf58b2663de11d525a05d6e5a9ce4b66fff10d
Gabriel Augendre <gabriel@augendre.info>
2015-03-27 11:25:42 +0100

    Proposition nouvelle correction

--- source/_static/donnees/corrections/get/index.php
+++ source/_static/donnees/corrections/get/index.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="style.css">
-		<title>Exercice Get</title>
-	</head>
-	<body>
-		<?php
-		
-			include('liste_hello.php');
-		
-			if(isset($_GET['nb_hello'])){
-				listeHello((int) $_GET['nb_hello']);
-			}
-		?>
-	</body>
+<head lang="fr">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../../assets/style.css"/>
+    <link rel="stylesheet" href="assets/style.css"/>
+    <title>Pizzas !</title>
+</head>
+<body>
+<?php
+require_once("hello.php");
+
+liste_hello($_GET['nb_hello']);
+?>
+</body>
 </html>
